b560bfbe0f7169e2a3812d4988ce164004aeaa6d
caglanurgg <caglanurgg@gmail.com>
2023-09-06 23:38:42 +0300

    lessons

--- src/day45_maps/C08_MapMethodlari.java
+++ src/day45_maps/C08_MapMethodlari.java
@@ -17,34 +17,36 @@ import java.util.Map;
             System.out.println(harfler); // {A=15, T=5, Z=50, K=25, L=10}
 
             System.out.println(harfler.putIfAbsent("L", 30)); // 10
-            System.out.println(harfler);
+            //putIfAbsent yoksa ekle demek
+            System.out.println(harfler); //{A=15, T=5, Z=50, K=25, L=10}
 
-            // T'nin value'sunu 2 katina cikaralim
+            // Q1) T'nin value'sunu 2 katina cikaralim
 
+            // once T'nin var olan degerini almamiz lazim
             int value = harfler.get("T"); // 5
 
             harfler.put("T", 2*value);
-
             System.out.println(harfler); // {A=15, T=10, Z=50, K=25, L=10}
 
-            // Z'nin degerini 20 artir
+            // Q2) Z'nin degerini 20 artir
 
-            harfler.compute("Z",(k,v)-> v+20);
+            harfler.compute("Z",(k,v)-> v+20); // compute hesapla
             System.out.println(harfler); // {A=15, T=10, Z=70, K=25, L=10}
 
-            // L'nin degerini 3 katinin 5 fazlasi yapin
+            // Q3) L'nin degerini 3 katinin 5 fazlasi yapin
 
             harfler.compute("L", (a,b) -> 3*b +5 );
+            // L a ve b den olusur (a = key ,b = value)  3*10+5
             System.out.println(harfler); // {A=15, T=10, Z=70, K=25, L=35}
 
-            // map'de M yoksa degeri 50 olarak map'e ekleyin
+            // Q4) map'de M yoksa degeri 50 olarak map'e ekleyin
 
-            // harfler.putIfAbsent("M",50);
-            harfler.computeIfAbsent("M",v -> 50);
+            // harfler.putIfAbsent("M",50); bu da olur
+            harfler.computeIfAbsent("M",v -> 50); // bu da olur
 
             System.out.println(harfler); // {A=15, T=10, Z=70, K=25, L=35, M=50}
 
-            // T harfi varsa value'sunu bir artirin
+            // Q5) T harfi varsa value'sunu bir artirin
 
             harfler.computeIfPresent("T" , (k,v) -> v+1);
             System.out.println(harfler); // {A=15, T=11, Z=70, K=25, L=35, M=50}
